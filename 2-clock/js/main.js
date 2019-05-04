fef9f57f025fec18d8e42411f3d0b7085577cf24
justynamak <justyna.zdunkowska@gmail.com>
2019-05-04 22:24:07 +0200

    initialize js css clock

---
+++ 2-clock/js/main.js
@@ -0,0 +1,22 @@
+const secondHand = document.querySelector(".second-hand");
+const minutesHand = document.querySelector(".min-hand");
+const hoursHand = document.querySelector(".hour-hand");
+
+const setDate = function () {
+    const time = new Date();
+    const seconds = time.getSeconds();
+    const minutes = time.getMinutes();
+    const hours = time.getHours();
+
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    const hoursDegrees = ((minutes / 12) * 360) + 90;
+
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+
+}
+
+setInterval(setDate, 1000);
