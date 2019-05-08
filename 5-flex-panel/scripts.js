bd11d4934c2e318bb7df708c9dc784892321b7f6
justynamak <justyna.zdunkowska@gmail.com>
2019-05-08 22:54:58 +0200

    czyszczenie klas

--- 5-flex-panel/scripts.js
+++ 5-flex-panel/scripts.js
@@ -1,15 +1,22 @@
 const panels = document.querySelectorAll(".panel");
 
+const removeClasses = function () {
+    panels.forEach(panel => {
+        if (panel.classList.contains(".open-active")) {
+            panel.classList.remove("open-active")
+        }
+    });
+}
 const toggleOpen = function () {
     this.classList.toggle("open");
 };
 
 const toggleOpenActive = function (e) {
-    console.log(e.propertyName);
     if (e.propertyName.includes("flex")) {
         this.classList.toggle("open-active");
     }
 };
 
+window.addEventListener("DOMContentLoaded", removeClasses);
 panels.forEach(panel => panel.addEventListener("click", toggleOpen));
 panels.forEach(panel => panel.addEventListener("transitionend", toggleOpenActive));
